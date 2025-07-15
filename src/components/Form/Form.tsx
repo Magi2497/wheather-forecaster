6bfcc88b025fa6004fe2b88b9780f373e66b7f9d
Magi2497 <magipayerasasndreu@gmail.com>
2025-07-15 19:05:13 +0200

    added styles to form and  function on change to modify the inputs

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,46 @@
+import { useState } from 'react'
+import type { SearchType } from '../../types'
 import { countries } from '../../data/countries'
 import styles from './Form.module.css'
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: '',
+  })
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   return (
     <>
       <form className={styles.form}>
         <div className={styles.field}>
           <label htmlFor="city"> City:</label>
-          <input type="text" id="city" name="city" placeholder="City" />
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="Type a city"
+            value={search.city}
+            onChange={handleChange}
+          />
         </div>
         <div className={styles.field}>
           <label htmlFor="country"> Country: </label>
-          <select name="country" id="country">
+          <select
+            name="country"
+            id="country"
+            value={search.country}
+            onChange={handleChange}
+          >
             <option value=""> --Select one Country --</option>
             {countries.map(country => (
               <option key={country.code} value={country.code}>
@@ -19,7 +49,11 @@ export default function Form() {
             ))}
           </select>
 
-          <input type="submit" value="Consult Wheater" />
+          <input
+            type="submit"
+            className={styles.submit}
+            value="Consult Wheater"
+          />
         </div>
       </form>
     </>
