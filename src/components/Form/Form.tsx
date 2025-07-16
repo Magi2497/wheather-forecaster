9af7f5b6750cfc39de35545678270fda5aed8a7c
Magi2497 <magipayerasasndreu@gmail.com>
2025-07-16 16:10:14 +0200

    added form valildation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import type { SearchType } from '../../types'
 import { countries } from '../../data/countries'
 import styles from './Form.module.css'
+import Alert from '../Alert/Alert'
 export default function Form() {
   const [search, setSearch] = useState<SearchType>({
     city: '',
     country: '',
   })
-
+  const [alert, setAlert] = useState('')
   const handleChange = (
     e:
       | React.ChangeEvent<HTMLInputElement>
@@ -19,9 +20,19 @@ export default function Form() {
     })
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (Object.values(search).includes('')) {
+      setAlert('All the fields are required')
+    }
+  }
+
   return (
     <>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        {alert && <Alert>{alert}</Alert>}
+
         <div className={styles.field}>
           <label htmlFor="city"> City:</label>
           <input
