55578b1a0d43516c02aaffc8449f5d1e76c0262b
Magi2497 <magipayerasasndreu@gmail.com>
2025-07-21 16:39:20 +0200

    added type guard to weather

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,19 @@
 import axios from 'axios'
-import type { SearchType } from '../types'
+import type { SearchType, Weather } from '../types'
+
+// type guard or assertion
+function isWheaterResponse(weather: unknown): weather is Weather {
+  const w = weather as Weather
+
+  return (
+    Boolean(weather) &&
+    typeof weather === 'object' &&
+    typeof w.name === 'string' &&
+    typeof w.main.temp === 'number' &&
+    typeof w.main.temp_max === 'number' &&
+    typeof w.main.temp_min === 'number'
+  )
+}
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -14,9 +28,18 @@ export default function useWeather() {
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-      const { data: weatherResult } = await axios(weatherUrl)
+      // Cast type
+      // const { data: weatherResult } = await axios<Weather>(weatherUrl)
+      // console.log(weatherResult.main.temp_max)
+      // console.log(weatherResult.name)
 
-      console.log(weatherResult)
+      // Type Guards
+      const { data: weatherResult } = await axios(weatherUrl)
+      const result = isWheaterResponse(weatherResult)
+
+      if (result) {
+        console.log(weatherResult.name)
+      }
     } catch (error) {
       console.log(error)
     }
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,13 @@ export type Country = {
   code: string
   name: string
 }
+
+export type Weather = {
+  name: string
+
+  main: {
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+}
