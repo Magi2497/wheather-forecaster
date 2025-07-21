e1a8f4c8c5f23e07908f5982906c1f827fd9c1b3
Magi2497 <magipayerasasndreu@gmail.com>
2025-07-21 18:34:56 +0200

    added styles to weather data

--- src/components/WeatherDetail/WeatherDetail.tsx
+++ src/components/WeatherDetail/WeatherDetail.tsx
@@ -1,5 +1,6 @@
 import { formatTemperature } from '../../helpers'
 import type { Weather } from '../../hooks/useWeather'
+import styles from './WeatherDetail.module.css'
 
 type WeatherDetailProps = {
   weather: Weather
@@ -7,12 +8,18 @@ type WeatherDetailProps = {
 
 export default function WeatherDetail({ weather }: WeatherDetailProps) {
   return (
-    <div>
-      <h2>Weather form: {weather.name}</h2>
-      <p className="">{formatTemperature(weather.main.temp)}&deg;C</p>
-      <div className="">
-        Min: <span>{formatTemperature(weather.main.temp_min)}&deg;C</span>
-        Max: <span>{formatTemperature(weather.main.temp_max)}&deg;C</span>
+    <div className={styles.container}>
+      <h2>Weather in {weather.name}</h2>
+      <p className={styles.current}>
+        {formatTemperature(weather.main.temp)}&deg;C
+      </p>
+      <div className={styles.temperatures}>
+        <p>
+          Min: <span>{formatTemperature(weather.main.temp_min)}&deg;C</span>
+        </p>
+        <p>
+          Max: <span>{formatTemperature(weather.main.temp_max)}&deg;C</span>
+        </p>
       </div>
     </div>
   )
